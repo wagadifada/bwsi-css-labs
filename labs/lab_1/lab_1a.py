d38e3619c484dce2d4da2ea3fb5d45cc9be8583a
wagadifada <gavinkoo2020@gmail.com>
2026-02-08 11:20:36 -0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Gavin Koo" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey everyone! My name is Gavin Koo, and I'm a junior from California. I love designing various things using CAD, especially when it comes to mechanical systems for my FRC team. I also have a keen interest in fluid dynamics and anything underwater, having used software like COMSOL and Ansys Fluent for both research and personal projects. Outside of academics, I enjoy swimming, playing the piano, and just messing around with my 3D printer. I look forward to meeting you all!")
 
 if __name__ == "__main__":
     main()
